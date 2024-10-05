7b26750cf33b79a49052b0f1c4dcb90b6166b8d3
Dany <dany.malchihin4@gmail.com>
2024-10-05 22:38:33 +0300

    python code reliz

--- arkanoid.cpp
+++ arkanoid.cpp
@@ -7,10 +7,26 @@ const char *password = "123456788";
 
 WiFiServer server(80);
 
+unsigned long fastbreak = 0;
+unsigned long timer = 0;
+
+const int Motor1_1 = 1;
+const int Motor1_2 = 2;
+const int Motor2_1 = 3;
+const int Motor2_2 = 4;
+
 void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println("Configuring access point...");
+  
+   pinMode(Motor1_1, OUTPUT);
+  pinMode(Motor1_2, OUTPUT);
+  pinMode(Motor2_1, OUTPUT);
+  pinMode(Motor2_2, OUTPUT);
+  
+  //pinMode(relay1, OUTPUT);
+  //pinMode(relay2, OUTPUT);
 
   while (!WiFi.softAP(ssid, password)) {
     Serial.println("Soft AP creation failed.");
@@ -31,15 +47,58 @@ void loop() {
         uint8_t Buffer[4];
         client.read(Buffer, 4);
 
-        int speed = (Buffer[0] * 100) + (Buffer[1] * 10) + Buffer[2];
+        int motor = (Buffer[0] * 100) + (Buffer[1] * 10) + Buffer[2];
         int relay = Buffer[3];
 
-        Serial.print("Speed:");
-        Serial.println(speed);
+        Serial.print("motor:");
+        Serial.println(motor);
         Serial.print("Relay:");
         Serial.println(relay);
+
+        int speed_left = motor;
+        int speed_right = motor;
+
+        motors(motor);
+
+        if (relay == 1 && millis() - fastbreak >= 3000) {
+          if (millis() - timer >= 950) punch();
+          else digitalWrite(8, 1);
+        } else {
+          timer = millis();
+        }
+        delay(4);
       }
     }
   }
 }
 
+void motors(int motor) {
+  int speed_left = constrain(motor, -255, 255);
+  int speed_right = constrain(motor, -255, 255);
+
+  if (speed_left >= 0) {
+    digitalWrite(Motor1_1, LOW);
+    analogWrite(Motor1_2, speed_left);
+  } else {
+    digitalWrite(Motor1_1, HIGH);
+    analogWrite(Motor1_2, abs(speed_left));
+  }
+
+  if (speed_right >= 0) {
+    digitalWrite(Motor2_1, LOW);
+    analogWrite(Motor2_2, speed_right);
+  } else {
+    digitalWrite(Motor2_1, HIGH);
+    analogWrite(Motor2_2, abs(speed_right));
+  }
+}
+
+void punch() {
+  digitalWrite(8, 0);
+  delay(50);
+  digitalWrite(9, 1);
+  delay(50);
+  digitalWrite(9, 0);
+  timer = millis();
+  fastbreak = millis();
+}
--- test_udp.py
+++ test_udp.py
@@ -1,14 +1,17 @@
 import socket
 
-HOST = '192.168.4.1' 
-PORT = 80           
-while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    
-    speed = 12 
-    relay = 1   
-    
-    data = [speed // 100, (speed % 100) // 10, speed % 10, relay]
-    
-    s.sendall(bytes(data))
+def nope(x):
+    pass
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_name = ('192.168.4.1', 80)
+server_socket.connect(server_name)
+
+speed = -123
+solenoid_state = 0
+
+data = f"{speed},{solenoid_state}\n"
+
+server_socket.send(data.encode("utf-8"))
+
+server_socket.close()
