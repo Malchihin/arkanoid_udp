cbbdb79e232ece87081d412d9c14c6dcfa7cfbea
Dany <dany.malchihin4@gmail.com>
2024-10-06 17:40:22 +0300

    arduino motor

--- arkanoid.cpp
+++ arkanoid.cpp
@@ -7,7 +7,23 @@ const char *password = "123456788";
 
 WiFiServer server(80);
 
-void setup()  {
+unsigned long fastbreak = 0;
+unsigned long timer = 0;
+
+const int Motor1_1 = 1;
+const int Motor1_2 = 2;
+const int Motor2_1 = 3;
+const int Motor2_2 = 4;
+
+void setup() {
+  pinMode(Motor1_1, OUTPUT);
+  pinMode(Motor1_2, OUTPUT);
+  pinMode(Motor2_1, OUTPUT);
+  pinMode(Motor2_2, OUTPUT);
+  
+  //pinMode(relay1, OUTPUT);
+  //pinMode(relay2, OUTPUT);
+
   Serial.begin(115200);
   Serial.println();
   Serial.println("Configuring access point...");
@@ -31,11 +47,11 @@ void loop() {
         String data = client.readStringUntil('\n');
         int commaIndex = data.indexOf(',');
         if (commaIndex != -1) {
-          int speed = data.substring(0, commaIndex).toInt();
+          int motor = data.substring(0, commaIndex).toInt();
           int relay = data.substring(commaIndex + 1).toInt();
 
-          Serial.print("Speed: ");
-          Serial.println(speed);
+          Serial.print("Motor: ");
+          Serial.println(motor);
           Serial.print("Relay: ");
           Serial.println(relay);
         }
@@ -44,4 +60,35 @@ void loop() {
     client.stop();
     Serial.println("Client disconnected");
   }
+}
+
+void motors(int motor) {
+  int speed_left = constrain(motor, -255, 255);
+  int speed_right = constrain(motor, -255, 255);
+
+  if (speed_left >= 0) {
+    digitalWrite(Motor1_1, LOW);
+    analogWrite(Motor1_2, speed_left);
+  } else {
+    digitalWrite(Motor1_1, HIGH);
+    analogWrite(Motor1_2, abs(speed_left));
+  }
+
+  if (speed_right >= 0) {
+    digitalWrite(Motor2_1, LOW);
+    analogWrite(Motor2_2, speed_right);
+  } else {
+    digitalWrite(Motor2_1, HIGH);
+    analogWrite(Motor2_2, abs(speed_right));
+  }
+}
+
+void punch() {
+  digitalWrite(8, 0);
+  delay(50);
+  digitalWrite(9, 1);
+  delay(50);
+  digitalWrite(9, 0);
+  timer = millis();
+  fastbreak = millis();
 }
--- test_udp.py
+++ test_udp.py
@@ -7,8 +7,8 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_name = ('192.168.4.1', 80)
 server_socket.connect(server_name)
 
-speed = -123
-solenoid_state = 0
+speed = 1
+solenoid_state = 1
 
 data = f"{speed},{solenoid_state}\n"
 
